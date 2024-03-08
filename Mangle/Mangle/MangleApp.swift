a2a61340d7c984eacc4d8d86417cccd9bcaf8ab4
emmanuellf240 <162642732+emmanuellf240@users.noreply.github.com>
2024-03-07 19:55:30 -0600

    Add files via upload

---
+++ Mangle/Mangle/MangleApp.swift
@@ -0,0 +1,18 @@
+//
+//  MangleApp.swift
+//  Mangle
+//
+//  Created by ADMIN UNACH on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+
+struct MangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Inicio()
+        }
+    }
+}
